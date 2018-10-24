8fe6b439075695675b582c2c9491fb8f53129414
yachen16 <42549552+yachen16@users.noreply.github.com>
2018-10-24 08:28:41 -0700

    Update NamedCow.java

--- NamedCow.java
+++ NamedCow.java
@@ -1,14 +1,11 @@
-class Farm  {     
-  private Animal[] aBunchOfAnimals = new Animal[3];    
-  public Farm()     {       
-     aBunchOfAnimals[0] = new NamedCow("cow","Elsie","moo");          
-     aBunchOfAnimals[1] = new chick("chick","cheep","cluck");
-     aBunchOfAnimals[2] = new pig("pig","oink");    
-  }     
-  public void animalSounds()    {
-    for (int nI=0; nI < aBunchOfAnimals.length; nI++) {             
-       System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound() );       
-    }       
-       System.out.println( "The cow is known as " + ((NamedCow)aBunchOfAnimals[0]).getName() );    
-  } 
+class NamedCow extends cow{
+  private String myName;
+  public NamedCow(String type, String sound, String name){
+    myType = type;
+    mySound = sound;
+    myName = name;
+  }
+  public String getName(){
+    return myName;
+  }
 }
